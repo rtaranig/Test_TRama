73290e0082e32e12e12a814eafe400ff3714b33f
Rama T <rtaranig@gmail.com>
2012-05-14 14:28:47 -0700

    Second commit

---
+++ src/com/rama/hello/Hello.java
@@ -0,0 +1,10 @@
+package com.rama.hello;
+
+public class Hello {
+
+	public static String sayHello(String str) {
+		String msg = "Hello- " + str;
+		System.out.println("message " + msg);
+		return msg;
+	}
+}
